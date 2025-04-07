cb3c42f731c076342ff8b3df162aadc9ec157eda
iakigarci <garcianoya@hotmail.es>
2025-04-07 20:22:04 +0200

    fix websocket disconnect, refactor consumer in ws-producer

--- ws-producer/internal/domain/services/consumer/services/consumer/consumer.go
+++ ws-producer/internal/domain/services/consumer/services/consumer/consumer.go
@@ -2,10 +2,12 @@ package consumer
 
 import (
 	"context"
+	"real-time-messaging/producer/internal/domain/entities"
 	port "real-time-messaging/producer/internal/domain/ports"
 	"real-time-messaging/producer/pkg/logger"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"go.uber.org/zap"
 )
 
@@ -30,27 +32,72 @@ func (s *ConsumerService) Consume(c *gin.Context) error {
 	}
 
 	s.wsPort.AddConnection(conn)
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	if err := s.handleWebsocketConnection(ctx, conn, cancel); err != nil {
+		return err
+	}
+
+	<-ctx.Done()
+	return nil
+}
+
+func (s *ConsumerService) handleWebsocketConnection(ctx context.Context, conn *websocket.Conn, cancel context.CancelFunc) error {
 	messages, err := s.messageSubscriber.SubscribeMessage(ctx)
 	if err != nil {
 		s.logger.Error("failed to subscribe to message", zap.Error(err))
 		return err
 	}
 
-	go func() {
-		defer conn.Close()
-		for msg := range messages {
-			if err := s.wsPort.SendToAll(msg); err != nil {
-				s.logger.Error("failed to send message to websocket connections", zap.Error(err))
-				return
-			}
-			s.logger.Info("message published to all websocket connections", zap.Any("message", msg))
+	go s.handleClientMessages(conn, cancel)
+	go s.handleServerMessages(ctx, conn, messages, cancel)
+
+	return nil
+}
+
+func (s *ConsumerService) handleClientMessages(conn *websocket.Conn, cancel context.CancelFunc) {
+	for {
+		_, _, err := conn.ReadMessage()
+		if err != nil {
+			s.logger.Info("websocket client disconnected", zap.Error(err))
+			cancel()
+			return
 		}
+	}
+}
+
+func (s *ConsumerService) handleServerMessages(
+	ctx context.Context,
+	conn *websocket.Conn,
+	messages <-chan *entities.Message,
+	cancel context.CancelFunc,
+) {
+	defer func() {
+		conn.Close()
+		s.logger.Info("websocket connection closed")
 	}()
 
-	<-ctx.Done()
-	return nil
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case msg, ok := <-messages:
+			if !ok {
+				return
+			}
+
+			if err := s.wsPort.Send(conn, msg); err != nil {
+				s.logger.Error("failed to send message to websocket connection",
+					zap.Error(err),
+					zap.String("remote_addr", conn.RemoteAddr().String()))
+				cancel()
+				return
+			}
+
+			s.logger.Info("message sent to websocket connection",
+				zap.Any("message", msg),
+				zap.String("remote_addr", conn.RemoteAddr().String()))
+		}
+	}
 }
